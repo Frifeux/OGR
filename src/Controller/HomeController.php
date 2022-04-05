235facdf6f4758a52263f43f03f0bec0bb68c437
flavien <flavien.2001a@gmail.com>
2022-04-05 23:34:59 +0200

    Redirect base url('/') to login page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/home', name: 'app_home')]
     public function home(): Response
     {
         return $this->render('main_page/home.html.twig');
+    }
+
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_login');
     }
 }

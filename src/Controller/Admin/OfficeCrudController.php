ea3b40d7284dccbd1320f12dddffc57bdd13c290
flavien <flavien.2001a@gmail.com>
2022-04-19 11:36:21 +0200

    Add duplicate button for office object

--- src/Controller/Admin/OfficeCrudController.php
+++ src/Controller/Admin/OfficeCrudController.php
@@ -3,17 +3,29 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Office;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Translation\TranslatableMessage;
 
 class OfficeCrudController extends AbstractCrudController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     public static function getEntityFqcn(): string
     {
         return Office::class;
@@ -35,7 +47,7 @@ class OfficeCrudController extends AbstractCrudController
         // Ajout d'un bouton et d'un action custom pour renvoyer un mail de reinitialisation de MPD
         $duplicatingObject = Action::new('duplicatingObject', new TranslatableMessage('Dupliquer'), 'fa fa-copy')
             ->linkToCrudAction('duplicatingObject')
-            ->setCssClass('btn btn-warning');
+            ->addCssClass('text-warning');
 
         return $actions
             // Modification of the translation of the button
@@ -53,7 +65,7 @@ class OfficeCrudController extends AbstractCrudController
             })
 
             // Added the reset password button on the page
-            ->add(Crud::PAGE_DETAIL, $duplicatingObject);
+            ->add(Crud::PAGE_INDEX, $duplicatingObject);
     }
 
     public function configureFields(string $pageName): iterable
@@ -68,5 +80,25 @@ class OfficeCrudController extends AbstractCrudController
             TextField::new('department', new TranslatableMessage('Service')),
 
         ];
+    }
+
+    public function duplicatingObject(AdminContext $context, AdminUrlGenerator $adminUrlGenerator): Response
+    {
+        // Get the actual office's object class
+        $officeObjectToDuplicate = $context->getEntity()->getInstance();
+
+        // Creation of a URL to redirect the USER after he triggers the button
+        $url = $adminUrlGenerator->setController(self::class)
+            ->setAction(Action::INDEX)
+            ->generateUrl();
+
+        $office = clone $officeObjectToDuplicate;
+        $office->setName($office->getName() . ' (copie)');
+        $this->entityManager->persist($office);
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'Le bureau a bien été dupliqué');
+
+        return $this->redirect($url);
     }
 }

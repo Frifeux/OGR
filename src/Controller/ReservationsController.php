dfee0ac381750e088702d717410d51d4087be926
flavien <flavien.2001a@gmail.com>
2022-04-21 19:03:03 +0200

    Adding the possibility to remove a reservation

--- src/Controller/ReservationsController.php
+++ src/Controller/ReservationsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\EquipmentReservationRepository;
 use App\Repository\MeetingRoomReservationRepository;
 use App\Repository\OfficeReservationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,12 +15,17 @@ class ReservationsController extends AbstractController
     private EquipmentReservationRepository $equipmentReservationRepository;
     private MeetingRoomReservationRepository $meetingRoomReservationRepository;
     private OfficeReservationRepository $officeReservationRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(OfficeReservationRepository $officeReservationRepository, EquipmentReservationRepository $equipmentReservationRepository, MeetingRoomReservationRepository $meetingRoomReservationRepository)
+    public function __construct(OfficeReservationRepository      $officeReservationRepository,
+                                EquipmentReservationRepository   $equipmentReservationRepository,
+                                MeetingRoomReservationRepository $meetingRoomReservationRepository,
+                                EntityManagerInterface           $entityManager)
     {
         $this->officeReservationRepository = $officeReservationRepository;
         $this->equipmentReservationRepository = $equipmentReservationRepository;
         $this->meetingRoomReservationRepository = $meetingRoomReservationRepository;
+        $this->entityManager = $entityManager;
     }
 
     // TODO: TROUVER UN NOM DE ROUTE POUR CETTE ACTION
@@ -36,5 +42,26 @@ class ReservationsController extends AbstractController
             'equipmentReservations' => $equipmentReservations,
             'meetingRoomReservations' => $meetingRoomReservations,
         ]);
+    }
+
+    // Delete a reservation from user
+    #[Route('/reservation/delete/{id}', name: 'app_reservation_delete')]
+    public function delete(int $id): Response
+    {
+        $officeReservation = $this->officeReservationRepository->findOneBy(['user' => $this->getUser(), 'id' => $id]);
+        $equipmentReservation = $this->equipmentReservationRepository->findOneBy(['user' => $this->getUser(), 'id' => $id]);
+        $meetingRoomReservation = $this->meetingRoomReservationRepository->findOneBy(['user' => $this->getUser(), 'id' => $id]);
+
+        if ($officeReservation) {
+            $this->entityManager->remove($officeReservation);
+        } elseif ($equipmentReservation) {
+            $this->entityManager->remove($equipmentReservation);
+        } elseif ($meetingRoomReservation) {
+            $this->entityManager->remove($meetingRoomReservation);
+        }
+
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('app_reservation');
     }
 }
